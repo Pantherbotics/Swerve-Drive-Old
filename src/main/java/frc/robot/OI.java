6778a4cd6d81ccf43cafd3303bafec507ed67a5f
Nick J <jacquetnick8@gmail.com>
2018-10-29 16:31:14 -0700

    struffas

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,9 +1,9 @@
 package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
+//import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import frc.robot.Constants;
-import edu.wpi.first.wpilibj.buttons.Button;
-import frc.robot.commands.*;
+//import edu.wpi.first.wpilibj.buttons.Button;
+//import frc.robot.commands.*;
 
 public class OI{
     public Joystick stick = new Joystick(Constants.kJoyStick);
