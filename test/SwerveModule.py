94d06a658db9216280790735e99e5cfc8df2f629
Jake Moore <jakealexandermoore@icloud.com>
2018-11-07 16:56:18 -0800

    Jakes GUI Updates

--- test/SwerveModule.py
+++ test/SwerveModule.py
@@ -1,5 +1,8 @@
 import math
-
+import tkinter
+import tkinter as tk
+from tkinter import messagebox
+tkinter.messagebox
 
 class SwerveModule:
     def __init__(self, isReversed):
@@ -52,3 +55,44 @@ while abs(module.getModifiedError()) > 0.0001:
     count+=1
 
 print(count * 0.01, "seconds elapsed")
+
+
+#START OF JAKE'S Annoying GUI
+
+root = tkinter.Tk()
+
+def Test1():
+   messagebox.showinfo( "Hello From Python", "HI")
+
+frame1 = tk.Frame(root, width=120, height=120, background="red")
+frame2 = tk.Frame(root, width=110, height = 110, background="orange")
+frame3 = tk.Frame(root, width=100, height = 100, background="yellow")
+frame4 = tk.Frame(root, width=90, height = 90, background="green")
+frame5 = tk.Frame(root, width=80, height = 80, background="blue")
+frame6 = tk.Frame(root, width=70, height = 70, background="purple")
+frame7 = tk.Frame(root, width=60, height = 60, background="red")
+frame8 = tk.Frame(root, width=50, height = 50, background="orange")
+frame9 = tk.Frame(root, width=40, height = 40, background="yellow")
+frame10 = tk.Frame(root, width=30, height = 30, background="green")
+frame11 = tk.Frame(root, width=20, height = 20, background="blue")
+frame12 = tk.Frame(root, width=10, height = 10, background="purple")
+frame13 = tk.Button(root, width=1, height=2, background="purple", command = Test1)
+
+frame1.pack(fill=None, expand=True)
+frame2.pack(fill=None, expand=False)
+frame3.pack(fill=None, expand=False)
+frame4.pack(fill=None, expand=False)
+frame5.pack(fill=None, expand=False)
+frame6.pack(fill=None, expand=False)
+frame7.pack(fill=None, expand=False)
+frame8.pack(fill=None, expand=False)
+frame9.pack(fill=None, expand=False)
+frame10.pack(fill=None, expand=False)
+frame11.pack(fill=None, expand=False)
+frame12.pack(fill=None, expand=False)
+frame13.pack(fill=None, expand=False)
+
+#frame2.place(relx=0.5, rely=0.5, anchor="c")
+
+
+root.mainloop()
